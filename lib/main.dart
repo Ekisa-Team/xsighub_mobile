5c539ce3502e69f4693d7da8231c9eb3d27bd131
juanmesa2097 <juanmesa2097@gmail.com>
2023-06-06 01:26:07 -0500

    [revert] Set dev as default environment

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:xsighub_mobile/src/environment.dart';
 void main() {
   const String environment = String.fromEnvironment(
     'ENVIRONMENT',
-    defaultValue: Environment.envDevNetwork,
+    defaultValue: Environment.envDev,
   );
 
   Environment.init(environment);
